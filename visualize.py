03bb34cd6a34316f58292c1a27f5cb73bcfc6948
ajauhri <abhinavjauhri@gmail.com>
2013-11-11 19:01:45 -0800

    added 3d visualization

--- visualize.py
+++ visualize.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
+
+# for parsing csv file
 import csv
 from collections import defaultdict
+
+# for plotting
+import numpy as np
+from mpl_toolkits.mplot3d import axes3d
+import matplotlib.pyplot as plt
+
+# Pre-defined values
 CSV_FILE = 'data/aircraft_reports_AK_2013.csv' 
-DEBUG = 0
+LAT = 'Latitude'
+LONG = 'Longitude'
+ALT = 'Altitude(Ft)'
+DEBUG = 1
+
+def strToInt(l):
+	return [ float(x) for x in l ]
 
 def visualize():
 	columns = defaultdict(list)
@@ -12,17 +27,31 @@ def visualize():
 			for row in reader:
 				for (k,v) in row.items():
 					columns[k].append(v)
-		if len(columns['Latitude']) == 0:
+		if len(columns[LAT]) == 0:
 			raise Exception("Latitude list is empty")
-		if len(columns['Longitude']) == 0:
+		if len(columns[LONG]) == 0:
 			raise Exception("Longitude list is empty")
-		if len(columns['Altitude(Ft)']) == 0:
+		if len(columns[ALT]) == 0:
 			raise Exception("Altitude list is empty")
-		if DEBUG:
-			print columns['Latitude']
-			print columns['Longitude']
-			print columns['Altitude(Ft)']
 		print 'Done with parsing'
+		columns[LAT] = strToInt(columns[LAT])
+		columns[LONG] = strToInt(columns[LONG])
+		columns[ALT] = strToInt(columns[ALT])
+		X = np.asarray(columns[LAT])
+		Y = np.asarray(columns[LONG])
+		Z = np.asarray(columns[ALT])
+		if DEBUG:
+			print X
+			print Y
+			print Z
+		fig = plt.figure()
+		ax = fig.add_subplot(111, projection='3d')
+		ax.scatter(X, Y, Z, c='b', marker='^')
+		ax.set_xlabel(LAT)
+		ax.set_ylabel(LONG)
+		ax.set_zlabel(ALT)
+		plt.show()
+
 
 	except EnvironmentError, e: #parent of IOError, OSError 
 		print e 
