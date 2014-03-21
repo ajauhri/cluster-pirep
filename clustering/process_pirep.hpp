983f05b45702e6384cfae7e0731f48edd75a77d4
ajauhri <abhinavjauhri@gmail.com>
2014-03-21 13:34:49 -0400

    spherical to cartesian coordinates

--- clustering/process_pirep.hpp
+++ clustering/process_pirep.hpp
@@ -1,10 +1,16 @@
 #ifndef PROCESS_PIREP_INCLUDED
 #define PROCESS_PIREP_INCLUDED
 #define _USE_MATH_DEFINES
+#define _FEET_TO_NM 0.000164579 
 
+#include<cmath>
 #include<Eigen/Dense>
 #include<boost/date_time.hpp>
 #include<csv.hpp>
+/* Resources for converting spherical coordinates to cartesian cooordinates - 
+//http://rbrundritt.wordpress.com/2008/10/14/conversion-between-spherical-and-cartesian-coordinates-systems/
+https://stackoverflow.com/questions/1185408/converting-from-longitude-latitude-to-cartesian-coordinates  
+*/
 
 namespace bt = boost::posix_time;
 using Eigen::MatrixXd;
@@ -23,6 +29,16 @@ std::string erase_quotes(const std::string& str)
     stripped.erase(0, 1);
     stripped.erase(stripped.size() - 1);
     return stripped;
+}
+
+MatrixXd get_cartesian(float lat, float lon, float alt)
+{
+    MatrixXd r(3, 1);
+    float R = alt + 3440.06;
+    r(0, 0) = R * cos(lat) * cos(lon);
+    r(1, 0) = R * cos(lat) * sin(lon);
+    r(2, 0) = R * sin(lat);
+    return r;
 }
 
 MatrixXd process_pirep(const std::string& filename)
@@ -54,7 +70,8 @@ MatrixXd process_pirep(const std::string& filename)
         X(i, 0) = pt_to_time_t(pt);
         X(i, 1) = atof(erase_quotes(row[12]).c_str()) * M_PI / 180;
         X(i, 2) = atof(erase_quotes(row[13]).c_str()) * M_PI / 180;
-        X(i, 3) = atof(erase_quotes(row[14]).c_str());
+        X(i, 3) = atof(erase_quotes(row[14]).c_str()) * _FEET_TO_NM;
+        X.block(i, 1, 1, 3) = get_cartesian(X(i, 1), X(i, 2), X(i, 3)).transpose();
     }
     return X;
 }
